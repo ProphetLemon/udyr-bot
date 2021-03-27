791855f0b62155dc4f7ad222a9553a17221cb126
ProphetLemon <diegovidaldelrosal@gmail.com>
2021-03-27 17:09:38 +0100

    tictactoe

---
+++ comandos/jugar.js
@@ -0,0 +1,14 @@
+const {tictactoe} = require('reconlx');
+module.exports = {
+    name: 'jugar',
+    aliases: [],
+    description: 'Funcion para jugar al tres en raya',
+    async execute(message, args, cmd, client, Discord, profileData) {
+        const member = message.mentions.members.first();
+        if (!member) return metodosUltiles.insultar(message);
+        new tictactoe({
+            player_two:member,
+            message:message
+        })
+    }
+}
