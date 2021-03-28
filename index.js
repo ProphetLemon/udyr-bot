73c93d6cc47a2dab3df132eea7e3602e52084ed4
ProphetLemon <diegovidaldelrosal@gmail.com>
2021-03-28 13:44:27 +0200

    arreglo cambio de hora

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGODB_SRV,{
     console.log(err);
 });
 client.login(process.env.DISCORD_TOKEN);
-global.horasDiferencia = 0;
+global.horasDiferencia = -2;
 
 global.metodosUtiles ={
     numberWithCommas : function (x) {
