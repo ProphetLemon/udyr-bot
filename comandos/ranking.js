d3b7c20a7f2bc40ddafeb9026eb83b07c9ec67af
ProphetLemon <diegovidaldelrosal@gmail.com>
2021-04-03 13:29:43 +0200

    rank

--- comandos/ranking.js
+++ comandos/ranking.js
@@ -1,30 +1,31 @@
 global.personas = [];
 const profileModel = require('../models/profileSchema');
 module.exports = {
-   name: 'ranking',
+    name: 'ranking',
+    aliases: ['rank'],
     description: 'Funcion ver el raking de puntos',
-  async  execute(message,args,cmd,client,Discord,profileData) {
-         var personas = await profileModel.find();
-         personas.sort(function (a, b) {
+    async execute(message, args, cmd, client, Discord, profileData) {
+        var personas = await profileModel.find();
+        personas.sort(function (a, b) {
             return b.udyrcoins - a.udyrcoins;
         });
         var guildMembers = await message.guild.members.fetch();
         var mensaje = "";
         const newEmbed = new Discord.MessageEmbed()
             .setColor("#B17428")
-            .setAuthor(`🏆Ranking de udyr coins🏆`);            
-        for (let i = 0; i < personas.length; i++) {            
+            .setAuthor(`🏆Ranking de udyr coins🏆`);
+        for (let i = 0; i < personas.length; i++) {
             let competidor = personas[i];
-            if (i==2){
-                mensaje += `🥉 ${guildMembers.find(member=>competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>\n`;
-            }else if (i==1){
-                mensaje += `🥈 ${guildMembers.find(member=>competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>\n`;
-            }else if (i==0){
-                mensaje += `🥇 ${guildMembers.find(member=>competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>\n`;
-            }else if (i == personas.length - 1) {
-                mensaje += `💩 ${guildMembers.find(member=>competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>`;
-            }else {
-                mensaje += `${i + 1}.- ${guildMembers.find(member=>competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>\n`;
+            if (i == 2) {
+                mensaje += `🥉 ${guildMembers.find(member => competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>\n`;
+            } else if (i == 1) {
+                mensaje += `🥈 ${guildMembers.find(member => competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>\n`;
+            } else if (i == 0) {
+                mensaje += `🥇 ${guildMembers.find(member => competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>\n`;
+            } else if (i == personas.length - 1) {
+                mensaje += `💩 ${guildMembers.find(member => competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>`;
+            } else {
+                mensaje += `${i + 1}.- ${guildMembers.find(member => competidor.userID == member.id).displayName} - ${competidor.udyrcoins} <:udyrcoin:825031865395445760>\n`;
             }
         }
         newEmbed.setDescription(mensaje)
