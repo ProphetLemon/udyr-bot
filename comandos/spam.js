92b299fc9d3de975721397c167b29ac134798c91
ProphetLemon <diegovidaldelrosal@gmail.com>
2021-04-03 02:00:56 +0200

    actualizacion

--- comandos/spam.js
+++ comandos/spam.js
@@ -3,9 +3,9 @@ module.exports = {
     aliases: ['apuesta', 'cerrar'],
     description: 'Funcion para crear apuestas o apostar en ellas',
     execute(message, args, cmd, client, Discord, profileData) {
-        message.delete()
-        if ((!args[0] ||args[0].trim()=="") && message.author.id !="202065665597636609") return metodosUtiles.insultar(message);
-        return message.channel.send(`GENTE\n\n${message.member.displayName.toUpperCase()} ESTA EN PUTO DIRECTO\n\n`+
+        message.delete();
+        if ((!args[0] ||args[0].trim()=="" || !args[0].startsWith("https://www.twitch.tv/")) && message.author.id !="202065665597636609") return metodosUtiles.insultar(message);
+        return message.channel.send(`GENTE @everyone\n\n${message.member.displayName.toUpperCase()} ESTA EN PUTO DIRECTO\n\n`+
         `GO GO GO GO\n\n${message.author.id =="202065665597636609"?"https://www.twitch.tv/profetalimon":args[0]}`);
     }
 }
