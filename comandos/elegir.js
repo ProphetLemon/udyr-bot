b2877a0286234147eb3140f155b32c4aa549a137
ProphetLemon <51175253+ProphetLemon@users.noreply.github.com>
2021-09-25 18:29:15 +0200

    hotfix

--- comandos/elegir.js
+++ comandos/elegir.js
@@ -16,6 +16,11 @@ module.exports = {
     execute(message, args, cmd, client, Discord, profileData) {
         let opciones = message.content.split("\"")
         opciones.splice(0, 1);
+        for (let i = 0; i < opciones.length; i++) {
+            if (opciones[i].trim() == "") {
+                opciones.splice(i--, 1)
+            }
+        }
         if (opciones.length == 0) return message.channel.send("No has puesto ninguna opcion")
         if (opciones.length == 1) return message.channel.send("Solo has puesto una opcion")
         message.channel.send(`Tras mucho pensar, elijo:\n**${opciones[Math.floor(Math.random() * opciones.length)]}**`)
