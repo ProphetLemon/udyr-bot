8b037b35c8fa796706ef02bce00dc1d0b21a2cde
ProphetLemon <51175253+ProphetLemon@users.noreply.github.com>
2021-12-20 18:34:50 +0100

    inicializando recompensa diaria

--- eventos/guild/guildMemberAdd.js
+++ eventos/guild/guildMemberAdd.js
@@ -1,9 +1,12 @@
 const profileModel = require('../../models/profileSchema');
 
 module.exports = async (client, discord, member) => {
+    let ayer = new Date()
+    ayer.setDate(ayer.getDate() - 1)
     let profile = await profileModel.create({
         userID: member.id,
         serverID: member.guild.id,
+        dailyGift: ayer,
         coins: 1000
     });
     profile.save();
