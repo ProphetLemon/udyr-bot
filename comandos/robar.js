7680f5f0db741f792b960d659dc1cd1cf1924986
ProphetLemon <51175253+ProphetLemon@users.noreply.github.com>
2022-03-19 14:45:23 +0100

    hotfix

--- comandos/robar.js
+++ comandos/robar.js
@@ -16,7 +16,7 @@ module.exports = {
      * @param {*} profileData 
      */
     async execute(message, args, cmd, client, Discord, profileData) {
-        if (!profileData.udyrcoins) return message.reply("No tas inscrito en la Liga Udyr, maric\u00F3n. Haz un 'udyr puntos' antes")
+        if (!profileData) return message.reply("No tas inscrito en la Liga Udyr, maric\u00F3n. Haz un 'udyr puntos' antes")
         var hoy = getCETorCESTDate()
         if (profileData.robar && profileData.robar.getDate() == hoy.getDate()) {
             message.member.send("Ya has robado cabron").then(msg => {
