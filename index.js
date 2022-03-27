723ef72ed096a84051c8f1921802935b66f0fb09
ProphetLemon <51175253+ProphetLemon@users.noreply.github.com>
2022-03-27 12:24:39 +0200

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 const profileModel = require('./models/profileSchema');
 const fs = require('fs');
-global.moment = require('moment');
+global.moment = require('moment-timezone');
 moment.locale('es')
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
