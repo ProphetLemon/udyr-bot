1de07d1f96c54088c605e568803fa89b1d4acb3a
ProphetLemon <51175253+ProphetLemon@users.noreply.github.com>
2022-03-28 00:31:16 +0200

    mejora comando ping

--- comandos/ping.js
+++ comandos/ping.js
@@ -8,9 +8,11 @@ module.exports = {
      * @param {Message} message
      * @param {string[]} args
      */
-    execute(message, args, cmd, client, Discord, profileData) {  
-        console.log("INICIO PING");     
-        message.channel.send(`🏓 Hay una latencia de ${Date.now()-message.createdTimestamp} ms`);
+    execute(message, args, cmd, client, Discord, profileData) {
+        console.log("INICIO PING");
+        var reykjavik = moment.tz(moment().format("YYYY-MM-DD HH:mm:ss"), "Atlantic/Reykjavik")
+        var dateNow = new Date(reykjavik.clone().tz("Europe/Madrid").format(`MMMM DD, YYYY HH:mm:ss`))
+        message.channel.send(`🏓 Hay una latencia de ${dateNow - message.createdTimestamp} ms\nLa fecha del servidor es ${dateNow.getDate()}/${dateNow.getMonth() + 1} ${dateNow.getHours()}:${dateNow.getMinutes()}`);
         console.log("FIN PING");
     }
 }
