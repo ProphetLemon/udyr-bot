2934c717c954e5896ebfcad89479b2302c92632a
ProphetLemon <51175253+ProphetLemon@users.noreply.github.com>
2022-03-27 13:20:38 +0200

    entender cosas

--- comandos/robar.js
+++ comandos/robar.js
@@ -27,7 +27,7 @@ module.exports = {
         }
         var reykjavik = moment.tz(moment().format(), "Atlantic/Reykjavik")
         var madrid = reykjavik.clone().tz("Europe/Madrid")
-        console.log(madrid.toDate())
+        console.log(moment().format() + "\n" + madrid.toDate())
         return
         if (profileData.robar && profileData.robar.getDate() == hoy.getDate()) {
             message.member.send("Ya has robado cabron").then(msg => {
