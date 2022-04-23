f56f17a241fd321840779b2a923ccfc1506ad31e
ProphetLemon <51175253+ProphetLemon@users.noreply.github.com>
2022-04-23 13:29:30 +0200

    ahora saluda a los nuevos

---
+++ eventos/guild/guildMemberAdd.js
@@ -0,0 +1,13 @@
+const { Client, GuildMember } = require("discord.js")
+/**
+ * 
+ * @param {*} Discord 
+ * @param {Client} client 
+ * @param {GuildMember} member
+ */
+module.exports = (Discord, client, member) => {
+    if (!member.user.bot) {
+        const channel = client.channels.cache.get("598896817161240663")
+        channel.send(`Ha llegado un nuevo maric\u00F3 al servidor, <@${member.id}>`)
+    }
+}
