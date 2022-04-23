c02ab2acbf022c82726fd48922ea0349188a0cd4
ProphetLemon <51175253+ProphetLemon@users.noreply.github.com>
2022-04-23 22:29:51 +0200

    que solo felicite en el server de tontitos

--- eventos/guild/guildMemberAdd.js
+++ eventos/guild/guildMemberAdd.js
@@ -6,7 +6,7 @@ const { Client, GuildMember } = require("discord.js")
  * @param {GuildMember} member
  */
 module.exports = (Discord, client, member) => {
-    if (!member.user.bot) {
+    if (!member.user.bot && member.guild.id == "598896817157046281") {
         const channel = client.channels.cache.get("598896817161240663")
         channel.send(`Ha llegado un nuevo maric\u00F3 al servidor, <@${member.id}>`)
     }
