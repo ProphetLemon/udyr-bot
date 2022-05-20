5a597214deffe7a975dbccba0936fd89dfb70a34
ProphetLemon <51175253+ProphetLemon@users.noreply.github.com>
2022-05-20 20:46:22 +0200

    osea bien, no? tirando a grande

--- eventos/guild/messageCreate.js
+++ eventos/guild/messageCreate.js
@@ -85,21 +85,45 @@ async function felicitarDia(message) {
     }
 }
 
-
+/**
+ * 
+ * @param {Message} message 
+ * @returns 
+ */
 function ruleta(message) {
     if (message.content.charAt(message.content.length - 1) == '5' || message.content.slice(message.content.length - 5, message.content.length).trim() == "cinco") {
         message.reply("por el culo te la hinco, maric\u00F3n");
         return;
     }
-    if (message.content.trim().toLowerCase() == "vaporeon") {
-        message.channel.send("Hola chicos, ¿sabíais que en lo que respecta a la cría de Pokémon machos y hembras, Vaporeon es el Pokémon más compatible para los humanos? No sólo pertenecen al grupo de los huevos de campo, que se compone principalmente de mamíferos, sino que Vaporeon mide una media de 1 m y pesa 29 kg, lo que significa que son lo suficientemente grandes como para poder manejar pollas humanas, y con sus impresionantes estadísticas base de PS y el acceso a Armadura Ácida, puedes ser duro con uno. Debido a su biología basada principalmente en el agua, no me cabe duda de que un Vaporeon excitado estaría increíblemente húmedo, tanto que podrías tener sexo con uno durante horas sin que te doliera. Además, pueden aprender los movimientos Atracción, Ojitos Tiernos, Cautivar, Encanto y Látigo, y no tienen pelaje para ocultar los pezones, por lo que sería increíblemente fácil que uno te pusiera palote. Con sus habilidades Absorbe Agua e Hidratación, pueden recuperarse fácilmente de la fatiga con suficiente agua. Ningún otro Pokémon se acerca a este nivel de compatibilidad. Además, como dato curioso, si le das lo suficiente, puedes hacer que tu Vaporeon se vuelva blanco. Vaporeon está literalmente hecho para la polla humana. Una estadística de defensa insana + un alto nivel de PS + el ataque Armadura Ácida significa que puede aguantar pollas todo el día, de todas las formas y tamaños, y seguir queriendo más.")
-        return
+    if (message.content.trim().toLowerCase().includes("vaporeon")) {
+        return message.channel.send("Hola chicos, ¿sabíais que en lo que respecta a la cría de Pokémon machos y hembras, Vaporeon es el Pokémon más compatible para los humanos?" +
+            " No sólo pertenecen al grupo de los huevos de campo, que se compone principalmente de mamíferos, sino que Vaporeon mide una media de 1 m y pesa" +
+            " 29 kg, lo que significa que son lo suficientemente grandes como para poder manejar pollas humanas, y con sus impresionantes estadísticas " +
+            "base de PS y el acceso a Armadura Ácida, puedes ser duro con uno. Debido a su biología basada principalmente en el agua," +
+            " no me cabe duda de que un Vaporeon excitado estaría increíblemente húmedo, tanto que podrías tener sexo con uno durante" +
+            " horas sin que te doliera. Además, pueden aprender los movimientos Atracción, Ojitos Tiernos, Cautivar, Encanto y Látigo," +
+            " y no tienen pelaje para ocultar los pezones, por lo que sería increíblemente fácil que uno te pusiera palote. Con sus" +
+            " habilidades Absorbe Agua e Hidratación, pueden recuperarse fácilmente de la fatiga con suficiente agua. Ningún otro " +
+            "Pokémon se acerca a este nivel de compatibilidad. Además, como dato curioso, si le das lo suficiente, " +
+            "puedes hacer que tu Vaporeon se vuelva blanco. Vaporeon está literalmente hecho para la polla humana." +
+            " Una estadística de defensa insana + un alto nivel de PS + el ataque Armadura Ácida significa que" +
+            " puede aguantar pollas todo el día, de todas las formas y tamaños, y seguir queriendo más.")
+    }
+    var condiciones = ["tamaño", "grande", "pequeño", "estandar", "normalito"]
+    var test = condiciones.some(el => message.content.trim().toLowerCase().includes(el))
+    if (test) {
+        return message.channel.send({
+            content: "Oye ya que hablamos sobre tamaños que os parece de tamaño?\nOsea normal tirando a tamaño medio no?la mano es grande sabes?Xd", files: [{
+                attachment: "./images/estandar.jpg",
+                name: 'estandar.jpg'
+            }]
+        })
     }
     if (message.content.trim().toLowerCase() == "a" || message.content.trim().toLowerCase() == "ª") {
         message.channel.send("https://c.tenor.com/K_fk1dEUTzcAAAAC/mister-jagger-a.gif")
         return
     }
-    if (message.content.trim().toLowerCase() == "vikingos") {
+    if (message.content.trim().toLowerCase().includes("vikingos")) {
         message.channel.send({
             files: [{
                 attachment: "./images/vikingos.gif",
