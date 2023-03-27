0877852513b63dd68f026fc594f632e9f5f9067b
ProfetaLimon <diegovidaldelrosal@gmail.com>
2023-03-27 11:21:41 +0200

    fix

--- comandos/play.js
+++ comandos/play.js
@@ -3,44 +3,9 @@ const Discord = require('discord.js')
 const { Player } = require('discord-player');
 const play = require('play-dl')
 const players = new Map()
-/**
- * 
- * @param {Message} message 
- * @param {Client} client
- * @returns {Player} 
- */
-function getPlayer(message, client) {
-    var player = players.get(message.guildId)
-    if (!player) {
-        player = new Player(client)
-        // this event is emitted whenever discord-player starts to play a track
-        player.events.on('playerStart', async (queue, track) => {
-            // we will later define queue.metadata object while creating the queue
-            let yt_info = await play.video_info(track.url)
-            let thumbnail = yt_info.video_details.thumbnails[yt_info.video_details.thumbnails.length - 1].url
-            const songEmbed = new EmbedBuilder()
-                .setTitle(`:bear::guitar::notes: Está sonando **${track.title}** :notes::guitar:`)
-                .setImage(thumbnail)
-                .setColor("#c16a4f")
-                .setDescription(`Duración: ${track.duration}`)
-            player.channelText.send({ embeds: [songEmbed] })
-
-        });
-        player.events.on("disconnect", () => {
-            players.delete(message.guildId)
-        });
-        player.queues.create(message.member.guild, {
-            metadata: message.channel
-        })
-        player.channelText = message.channel
-        players.set(message.guildId, player)
-    }
-    return player
-}
-
 module.exports = {
     name: 'play',
-    aliases: ['skip'],
+    aliases: ['skip', 'leave', 'shuffle', 'bassboost', 'nightcore', 'earrape', '8d', 'bassboost_high', 'bassboost_low', 'vibrato', 'lofi', 'vaporwave'],
     description: 'Funcion ver el raking de puntos',
     /**
      * 
@@ -64,9 +29,31 @@ module.exports = {
                 }, 5000);
             })
         }
-        const player = getPlayer(message, client)
+        const player = new Player(client)
+        if (!players.get(client.user.id)) {
+            player.queues.create(message.member.guild, {
+                metadata: message.channel
+            })
+            player.channelText = message.channel
+            player.events.on('playerStart', async (queue, track) => {
+                // we will later define queue.metadata object while creating the queue
+                let yt_info = await play.video_info(track.url)
+                let thumbnail = yt_info.video_details.thumbnails[yt_info.video_details.thumbnails.length - 1].url
+                const songEmbed = new EmbedBuilder()
+                    .setTitle(`:bear::guitar::notes: Está sonando **${track.title}** :notes::guitar:`)
+                    .setImage(thumbnail)
+                    .setColor("#c16a4f")
+                    .setDescription(`Duración: ${track.duration}`)
+                player.channelText.send({ embeds: [songEmbed] })
+
+            });
+            players.set(client.user.id, "piola")
+        }
+
         if (cmd == "skip") return skip_song(player, message)
         if (cmd == "leave") return leave(player, message)
+        if (cmd == "shuffle") return shuffle(player, message)
+        if (cmd == "bassboost" || cmd == "nightcore" || cmd == "earrape" || cmd == "8d" || cmd == "bassboost_low" || cmd == "bassboost_high" || cmd == "vibrato" || cmd == "lofi" || cmd == "vaporwave") return filtro(player, message, cmd)
         let ytSearch = args.join(" ")
         var authorId = message.author.id
         if (play.yt_validate(ytSearch) == "video") {
@@ -74,12 +61,21 @@ module.exports = {
             await player.play(channel, yt_info.video_details.url)
             player.channelText.send(`Se ha añadido correctamente la canción 🐻👍`)
         } else if (play.yt_validate(ytSearch) == "playlist") {
+            var msgEdit
+            await player.channelText.send("Espere: Cargadas **0** canciones...").then(msg => {
+                msgEdit = msg
+            })
             let yt_info = await play.playlist_info(ytSearch)
             let videos = await yt_info.all_videos()
+            var cont = 0
             for (let i = 0; i < videos.length; i++) {
-                await player.play(channel, videos[i])
+                await player.play(channel, videos[i].url)
+                cont++
+                if (cont % 5 == 0) {
+                    await msgEdit.edit(`Espere: Cargadas **${cont}** canciones...`)
+                }
             }
-            player.channelText.send(`Se han añadido correctamente las canciones 🐻👍`)
+            msgEdit.edit(`Se han añadido correctamente las canciones 🐻👍`)
         } else if (play.sp_validate(ytSearch) == "track") {
             if (play.is_expired()) {
                 await play.refreshToken() // This will check if access token has expired or not. If yes, then refresh the token.
@@ -147,6 +143,44 @@ module.exports = {
  */
 function skip_song(player, message) {
     player.queues.get(message.guildId).node.skip()
+}
+
+/**
+ * 
+ * @param {Player} player 
+ * @param {Message} message
+ */
+function filtro(player, message, cmd) {
+    player.queues.get(message.guildId).filters.ffmpeg.toggle([cmd == '8d' ? '8D' : cmd])
+    message.channel.send("Filtro aplicado")
+}
+
+/**
+ * 
+ * @param {Player} player 
+ * @param {Message} message
+ */
+function shuffle(player, message) {
+    player.queues.get(message.guildId).tracks.shuffle()
+    message.channel.send("Canciones shuffleadas 🐻👍")
+}
+
+function shuffle_array(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
 }
 
 /**
